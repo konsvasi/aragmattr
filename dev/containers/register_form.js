9f41d66948273e84760884cf0cd0f5e34f26965b
Kostas V <konsvass@outlook.com>
2017-08-11 11:14:20 +0200

    added password confirmation

--- dev/containers/register_form.js
+++ dev/containers/register_form.js
@@ -7,44 +7,30 @@ class RegisterForm extends Component {
 
     this.state = {
       username: '',
-      password: ''
-    }
-
-    this.handleNameChange = (ev) => {
-      this.setState({username: ev.target.value});
-    }
-
-    this.handlePasswordChange = (ev) => {
-      this.setState({password: ev.target.value});
+      password: '',
+      confPwd: ''
     }
 
     this.handleSubmit = (ev) => {
       ev.preventDefault();
-      //this.props.handleSubmit(this.state.name, this.state.location);
-      // fetch('/', {
-      //   method: 'POST',
-      //   body: {
-      //     username: this.state.username,
-      //     password: this.state.password
-      //   }
-      // })
-      // .then(function(response){
-      //   return response.json()
-      // }).then(function(body) {
-      //   console.log('body:', body);
-      // })
-      axios.post('/', {
-          username: this.state.username,
-          password: this.state.password
-      })
-      .then((response) => {
-        console.log('response:', response);
-      })
-      .catch((error) => {
-        console.log('error:', error)
-      })
-      this.setState({username: ''});
-      this.setState({password: ''});
+      console.log('state on submit:', this.state);
+      if (this.state.password !== this.state.confPwd) {
+        console.error("Passwords don't match");
+      } else {
+        axios.post('/', {
+            username: this.state.username,
+            password: this.state.password
+        })
+        .then((response) => {
+          console.log('response:', response);
+        })
+        .catch((error) => {
+          console.log('error:', error)
+        })
+        this.setState({username: ''});
+        this.setState({password: ''});
+        this.setState({confPwd: ''});
+      }
     }
   }
 
@@ -56,14 +42,23 @@ class RegisterForm extends Component {
           <div className="form-group">
             <label htmlFor="username" className="sr-only">Email address:</label>
             <input type="Username" value={this.state.username}
-              onChange={this.handleNameChange}
+              required
+              onChange = {(ev) => this.setState({username: ev.target.value})}
               className="form-control" id="email" placeholder="Username"/>
           </div>
           <div className="form-group">
             <label htmlFor="pwd" className="sr-only">Password:</label>
             <input type="password" value={this.state.password}
-              onChange={this.handlePasswordChange}
+              required
+              onChange = {(ev) => this.setState({password: ev.target.value})}
               className="form-control" id="pwd" placeholder="Password"/>
+          </div>
+          <div>
+            <label htmlFor="confPwd" className="sr-only">Confirm password:</label>
+            <input type="password" value={this.state.confPwd}
+              required
+              onChange = {(ev) => this.setState({confPwd: ev.target.value})}
+              className="form-control" id="confPwd" placeholder="Confirm password"/>
           </div>
           <button type="submit" className="btn btn-default submit-button">Create account</button>
         </form>
